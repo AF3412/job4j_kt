9c489a48ae3e12d54c873c898b20bbf456e76d61
A F <af3412@yandex.ru>
2024-07-24 11:00:32 +0400

    add search max

--- src/main/kotlin/base/Calc.kt
+++ src/main/kotlin/base/Calc.kt
@@ -5,6 +5,10 @@ fun substract(first: Int, second: Int): Int = first - second
 fun multiply(first: Int, second: Int): Int = first * second
 fun divide(first: Int, second: Int): Int = first / second
 
+fun max2(first: Int, second: Int): Int = if (first > second) first else second
+
+fun max3(first: Int, second: Int, third: Int): Int = max2(max2(first, second), third)
+
 fun main(args: Array<String>) {
     println("1 + 1 = ${add(1, 1)}")
     println("1 - 1 = ${substract(1, 1)}")
--- src/test/kotlin/base/CalcTest.kt
+++ src/test/kotlin/base/CalcTest.kt
@@ -2,10 +2,7 @@ package base
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import ru.af3412.base.add
-import ru.af3412.base.divide
-import ru.af3412.base.multiply
-import ru.af3412.base.substract
+import ru.af3412.base.*
 
 class CalcTest {
 
@@ -27,5 +24,15 @@ class CalcTest {
     @Test
     fun when9Divide3() {
         assertThat(divide(9, 3)).isEqualTo(3)
+    }
+
+    @Test
+    fun whenMax2Compare() {
+        assertThat(max2(3,2)).isEqualTo(3)
+    }
+
+    @Test
+    fun whenMax3Compare() {
+        assertThat(max3(4,3, 2)).isEqualTo(4)
     }
 }
