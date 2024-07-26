447cbf04b4bb2ed426690f63c10a27c22c735af3
A F <af3412@yandex.ru>
2024-07-26 16:12:26 +0400

    update add example

--- src/main/kotlin/base/Calc.kt
+++ src/main/kotlin/base/Calc.kt
@@ -10,7 +10,7 @@ fun max2(first: Int, second: Int): Int = if (first > second) first else second
 fun max3(first: Int, second: Int, third: Int): Int = max2(max2(first, second), third)
 
 fun main(args: Array<String>) {
-    println("1 + 1 = ${add(1, 1)}")
+    println("1 + 2 = ${add(1, 1)}")
     println("1 - 1 = ${substract(1, 1)}")
     println("1 * 1 = ${multiply(1, 1)}")
     println("1 / 1 = ${divide(1, 1)}")
