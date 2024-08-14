5a9a15d2b0f77fe3f3f4bd34a41bbf7b1758db6d
A F <af3412@yandex.ru>
2024-08-14 10:32:25 +0400

    1. Вернитесь к заданию Tracker и добавьте необходимые модификаторы доступа.
    2. Залейте код и переведите ответственного на Петра Арсентьева.

--- src/main/kotlin/oop/tracker/Item.kt
+++ src/main/kotlin/oop/tracker/Item.kt
@@ -13,4 +13,17 @@ data class Item(
     fun addComment(comment: String) {
         comments[positionComment++] = comment
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Item
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id
+    }
 }
